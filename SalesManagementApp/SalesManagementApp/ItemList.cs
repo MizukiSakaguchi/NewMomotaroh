200c90866ea0e18ed781aad0524e96be9cdadcb0
Administrator <Administrator@WIN-65HEAO3ENOT>
2018-12-10 16:55:36 +0900

    ItemListたぶんできました

--- SalesManagementApp/SalesManagementApp/ItemList.cs
+++ SalesManagementApp/SalesManagementApp/ItemList.cs
@@ -12,16 +12,20 @@ namespace SalesManagementApp
 {
 
     /**
-     * label1.商品ID　2.商品名　3.カテゴリ　4.単価　11.在庫数     * 
+     * label1.商品ID　2.商品名　3.カテゴリ　4.単価　11.在庫数  
      */
     public partial class ItemList : Form
     {
+        private List<ItemDto> list = new List<ItemDto>();
+
         public ItemList()
         {
             InitializeComponent();
 
             ItemDao dao = new ItemDao();
-            dao.DisplayItem();
+            list = dao.DisplayItem();
+
+            list.ForEach(v => listBox1.Items.Add($"{v.Id}：{v.Name}"));
         }
                 
         //注文
@@ -45,10 +49,22 @@ namespace SalesManagementApp
             stock.Show();
         }
 
-        //商品リスト内のアイテムを選択したとき
+        /**
+         * リスト内の商品を選択したとき、選択された商品の情報を右側に表示するメソッド.         * 
+         * 
+         */
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            label1.Text = $"{list[listBox1.SelectedIndex].Id}";
+            label1.Visible = true;
+            label2.Text = $"{list[listBox1.SelectedIndex].Name}";
+            label2.Visible = true;
+            label3.Text = $"{list[listBox1.SelectedIndex].Category.Name}";
+            label3.Visible = true;
+            label4.Text = $"{list[listBox1.SelectedIndex].Price}";
+            label4.Visible = true;
+            label11.Text = $"{list[listBox1.SelectedIndex].Stock.Num}";
+            label11.Visible = true;                        
         }
     }
 }

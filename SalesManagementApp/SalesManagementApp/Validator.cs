46a5f9b44f3f97e1686fb7d1849e629677d66ffa
Administrator <Administrator@WIN-65HEAO3ENOT>
2018-12-10 13:49:04 +0900

    途中です

---
+++ SalesManagementApp/SalesManagementApp/Validator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManagementApp
+{
+    class Validator
+    {
+        public bool SetOn(string str)
+        {
+
+        }
+
+        public bool IsMinus(int val)
+        {
+
+        }
+
+        public bool IsPoint(int val)
+        {
+
+        }
+
+        public bool IsNumeric(string str)
+        {
+
+        }
+
+        public bool StockOver(int val)
+        {
+
+        }
+    }
+}

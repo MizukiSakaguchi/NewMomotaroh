294a1e591e8461f7a272a19364f203b77bd02d37
Administrator <Administrator@WIN-38E0D8OOTR3>
2018-12-10 16:38:09 +0900

    さくせいちう

--- SalesManagementApp/SalesManagementApp/CustomerDao.cs
+++ SalesManagementApp/SalesManagementApp/CustomerDao.cs
@@ -8,5 +8,60 @@ namespace SalesManagementApp
 {
     class CustomerDao
     {
+        List<CustomerDto> list = new List<CustomerDto>();
+
+        //データベースから顧客の一覧のリストを取ってきて返却するメソッド.
+         public List<CustomerDto> DisplayCustomer()
+        {
+            //データベースの接続開始  
+            SqlConnection con = new SqlConnection();
+
+            con.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings
+                ["SalesManagementApp.Properties.Settings.connectDB"].ConnectionString;
+
+            try
+            {
+                con.Open();
+
+                SqlCommand com = new SqlCommand();
+
+                //実行するSQL文の準備
+                com.CommandText = $"SELECT * FROM CustomerTable";
+
+                com.Connection = con;
+
+                SqlDataReader reader;
+                reader = command.ExecuteReader();
+
+            while (reader.Read())
+               {
+                list.Add(new CustomerDto(reader["CustID"].ToString() , reader["Name"].ToString() ,
+                reader["TEL"].ToString()));
+                reader.Close();
+                }
+
+                if(result > 0)
+                {
+                    return true;
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                try
+                {
+                    con.Close();
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                }
+            }
+            return list;
+      
+        }
     }
 }

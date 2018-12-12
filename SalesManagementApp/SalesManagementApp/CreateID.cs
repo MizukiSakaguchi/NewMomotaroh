c1615c6b7fa0584e39964ea18c46c3c158c33146
Administrator <Administrator@WIN-T2BMUF8KSM4>
2018-12-12 12:55:26 +0900

    createID

--- SalesManagementApp/SalesManagementApp/CreateID.cs
+++ SalesManagementApp/SalesManagementApp/CreateID.cs
@@ -32,8 +32,7 @@ namespace SalesManagementApp
             }
 
             SqlCommand command = new SqlCommand();
-
-            command.Connection = connection;
+            
 
             //検索結果取得用のオブジェクトを用意
             SqlDataReader reader = null;
@@ -81,7 +80,7 @@ namespace SalesManagementApp
         {
             string result = null;
 
-            SqlCommand command = new SqlCommand();
+            
             //DB接続
             SqlConnection connection = new SqlConnection();
             
@@ -105,12 +104,13 @@ namespace SalesManagementApp
             SqlDataReader sqlReader = null;
             try
             {
-                
+                SqlCommand command = new SqlCommand();
+                //Connection情報の登録
+                command.Connection = connection;
                 //実行するプロシージャの登録
                 command.CommandText = "SELECT * FROM StockTable ORDER BY StockID DESC;";
                 command.CommandType = CommandType.Text;
-                //Connection情報の登録
-                command.Connection = connection;
+                
                 //sqlの実行
                 sqlReader = command.ExecuteReader();
 
